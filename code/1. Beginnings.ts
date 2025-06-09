53649f83365d9ab073477da93af71948acf852a1
Aaditya Kumar Mittal <adityakumarashish1909@gmail.com>
2025-06-09 23:03:46 +0530

    init: Started Basic of Typescript for Nodejs

---
+++ code/1. Beginnings.ts
@@ -0,0 +1,7 @@
+console.log("Hello world!");
+
+function getName(name:string){
+  return name;
+}
+
+console.log(getName("Aaditya Kumar Mittal"));
---
+++ src/app.ts
@@ -0,0 +1,41 @@
+// Basic Types
+
+let isDone : Boolean = false
+let num: Number = 1000
+let str: String = "Hello World"
+
+let list : any[] = [1, 2, 3, "Apple","Grapes"]
+
+console.log(isDone)
+console.log(num)
+console.log(str)
+console.log(list)
+
+let randomVal : any = 4
+
+console.log(randomVal)
+
+randomVal ="Aaditya"
+
+console.log(randomVal)
+
+randomVal = true
+
+console.log(randomVal)
+
+let xyz :undefined = undefined
+let abc :null = null
+
+console.log(xyz)
+console.log(abc)
+
+enum Color {Red, Green, Blue}
+let c : Color = Color.Green
+
+console.log(c)
+
+// Tuple
+
+let tuple1: [string , number] = ["Aaditya", 1];
+
+console.log(tuple1);
